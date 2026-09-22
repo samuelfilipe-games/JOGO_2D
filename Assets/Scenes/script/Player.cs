2798a567f59c9f3b8c924d021a0c500791461bce
Thales <cdojoguinhos@gmail.com>
2026-09-22 09:28:44 -0300

    fizemos o player pular apenas no chão

--- Assets/Scenes/script/Player.cs
+++ Assets/Scenes/script/Player.cs
@@ -5,7 +5,8 @@ public class Player : MonoBehaviour
     public float speed = 5f;
     private Rigidbody2D rb;
 
-    
+    private bool isGrounded = false;
+
     void Start()
     {
        rb = GetComponent<Rigidbody2D>(); 
@@ -16,9 +17,25 @@ public class Player : MonoBehaviour
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         rb.linearVelocity = new Vector2( moveHorizontal * speed, rb.linearVelocity.y ); //esse é pra fazer o player andar na horizontal
-       if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rb.AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse); // esse é pra fazer player pular
+        }
+    }
+
+   void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true; //vai reconhecer quando o jogador pular no chão
+            
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+     if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false; 
         }
     }
 }
